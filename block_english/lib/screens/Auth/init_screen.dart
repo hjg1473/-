3416902e709cc4c3a819cdc3b4387a26501955db
parkjin21004 <parkjin21004@gmail.com>
2024-06-26 18:16:51 +0900

    Added Initial Page and Login Page

---
+++ block_english/lib/screens/Auth/init_screen.dart
@@ -0,0 +1,51 @@
+import 'package:block_english/utils/constants.dart';
+import 'package:block_english/widgets/round_corner_route_button.dart';
+import 'package:flutter/material.dart';
+
+class InitScreen extends StatelessWidget {
+  const InitScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.abc,
+              size: 300,
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: RoundCornerRouteButton(
+                text: "로그인",
+                routeName: '/login_screen',
+                width: 700,
+                heigth: 45,
+                type: ButtonType.filled,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: RoundCornerRouteButton(
+                text: "회원가입",
+                routeName: '/login_screen',
+                width: 700,
+                heigth: 45,
+                type: ButtonType.outlined,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ block_english/lib/screens/Auth/login_screen.dart
@@ -0,0 +1,58 @@
+import 'package:block_english/utils/constants.dart';
+import 'package:block_english/widgets/round_corner_route_button.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Column(
+          children: [
+            Icon(
+              Icons.abc,
+              size: 300,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 10.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: '아이디',
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 10.0),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: '비밀번호',
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: RoundCornerRouteButton(
+                text: "로그인",
+                routeName: '/init',
+                width: 700,
+                heigth: 45,
+                type: ButtonType.filled,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
