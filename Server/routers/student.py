b45fa7e7a5e9c2b4020d7711a36171b21c9b5f29
hjg1473 <hjg1473@naver.com>
2024-07-04 11:56:51 +0900

    feature/super/class_control

--- Server/routers/student.py
+++ Server/routers/student.py
@@ -32,7 +32,61 @@ templates = Jinja2Templates(directory="templates")
 
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
+
+
+# 학생과 선생님 연결 요청, 학생 -> 선생님(student_teachers) / 선생님 -> 학생(teachers_students) ?
+@router.get("/connecting", status_code = status.HTTP_200_OK)
+async def connect_teacher(
+    teacher_id: int,
+    user: dict = Depends(get_current_user),
+    db: Session = Depends(get_db)):
+
+    if user is None:
+        raise get_user_exception()
+
+    # 학생 정보 가져오기
+    student = db.query(Users).filter(Users.id == user.get("id")).first()
     
+    if not student:
+        raise get_user_exception()
+
+    if teacher_id == user.get("id"):
+        raise HTTPException(status_code=404, detail='자기 자신은 지정할 수 없습니다.')
+    
+    # 쿼리 파라미터로 받은 teacher_id 또는 teacher_username을 사용해 선생님 검색
+    teacher = db.query(Users).filter(Users.id == teacher_id).first()
+
+    if not teacher:
+        raise HTTPException(status_code=404, detail='선생님을 찾을 수 없습니다.')
+
+    # 학생과 선생님이 이미 연결되어 있는지 확인
+    if teacher in student.student_teachers:
+        return {"message": "Already connected"}
+
+    # 새로운 연결 생성
+    student.student_teachers.append(teacher)
+    # db.add(student) # ORM 방식은 append 할 때 자동으로 해줌
+    db.commit()
+    return {"message": "Connected successfully", "teacher_id": teacher.id, "teacher_username": teacher.username}
+
+# 학생(self)과 연결된 선생님 아이디 반환
+@router.get("/connect_teacher", status_code = status.HTTP_200_OK)
+async def read_user_all(user: user_dependency, db: db_dependency):
+    if user is None:
+        raise get_user_exception()
+    
+    # 쿼리 검색
+    teacher = db.query(Users).options( 
+        joinedload(Users.student_teachers)
+    ).filter(Users.id == user.get("id")).first()
+
+    # if not teacher.student_teachers: # 없으면 not found.
+    #     raise http_exception()
+    
+    # {"teachers": [{"id": teacher.id} for teacher in teacher.student_teachers]}
+
+    return {"teachers": [{"id": teacher.id} for teacher in teacher.student_teachers]}
+
 @router.get("/", status_code = status.HTTP_200_OK)
 async def read_user_all(user: user_dependency, db: db_dependency):
     if user is None:
@@ -51,17 +105,14 @@ async def read_user_all(user: user_dependency, db: db_dependency):
 
     # Join 해서 학습 정보를 반환해야됨. + 노출되는 정보 필터링
 
+# 사용자의 id 반환, self
 @router.get("/id", status_code = status.HTTP_200_OK)
 async def read_user_id(user: user_dependency, db: db_dependency):
     if user is None:
         raise get_user_exception()
     
-    user_id = db.query(Users.id).filter(Users.id == user.get('id')).first()[0]
-    user_id_json = { "id": user_id }
-    return user_id_json
-    # 필터 사용. 학습 정보의 owner_id 와 '유저'의 id가 같으면,
+    return  {"id": user.get('id')}
     # 사용자의 id 반환.
-
 
 # 학생의 self 학습 정보 반환.
 @router.get("/studyinfo", status_code = status.HTTP_200_OK)
@@ -76,10 +127,15 @@ async def read_studyinfo_all(user: user_dependency, db: db_dependency):
         joinedload(StudyInfo.incorrect_problems)
     ).filter(StudyInfo.id == user.get("id")).first()
 
-
-    if study_info is None:
-        return {'detail':'학습한 정보가 없습니다.'}
-
+    # 초기화
+    correct_problems_type1_count = 0
+    correct_problems_type2_count = 0
+    correct_problems_type3_count = 0
+    incorrect_problems_type1_count = 0
+    incorrect_problems_type2_count = 0
+    incorrect_problems_type3_count = 0
+    
+    # 조금 수정을 원해
     if study_info:
         correct_problems_type1_count = sum(1 for problem in study_info.correct_problems if problem.type == '부정문')
         correct_problems_type2_count = sum(1 for problem in study_info.correct_problems if problem.type == '의문문')
