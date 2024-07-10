ce0c548eb6e21303f520799b98675dc168479866
hjg1473 <hjg1473@naver.com>
2024-07-10 14:51:06 +0900

    feature/game/problem_solve,score

--- Server/routers/game.py
+++ Server/routers/game.py
@@ -37,12 +37,13 @@ db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]    
 
 
-# 방 객체 ?
+# 방 객체 
 class Room:
-    def __init__(self, room_id: str, host_id: str):
-        self.room_id = room_id 
+    def __init__(self, room_id: str, host_id: str, room_max: int):
+        self.room_id = room_id
         self.host_id = host_id
-        self.participants: List[str] = [] # 참여자 리스트
+        self.room_max = room_max
+        self.participants: Dict[str, int] = {} # 참여자 리스트 -> 딕셔너리 (개인 점수 계산용)
         self.host_websocket: WebSocket = None # 웹소켓
 
 rooms: Dict[str, Room] = {}
@@ -51,11 +52,15 @@ rooms: Dict[str, Room] = {}
 class CreateRoomRequest(BaseModel):
     host_id: str
     cproblem_id: int
+    room_max: int
 
 # 참여자가 방 참가할 때 쓰는 객체
 class JoinRoomRequest(BaseModel):
     room_id: str
     participant_id: str
+
+class GetStudentScoreRequest(BaseModel):
+    room_id: str
 
 class ParticipantSolveRequest(BaseModel):
     room_id: str
@@ -76,7 +81,7 @@ class ConnectionManager:
         self.active_connections[room_id].pop(client_id, None)
 
     async def send_personal_message(self, message: str, websocket: WebSocket): # 
-        await websocket.send_text(message)
+        await websocket.send_text(json.dumps(message))
 
 manager = ConnectionManager()
 
@@ -91,6 +96,14 @@ custom_problem = {
     "543533": ["I am pretty.", "I am pretty.", "I am pretty."],
     "123232": ["I am pretty.", "I am pretty.", "I am pretty."]
 }
+
+# 게임방 호스트가 참여자의 모든 누적 점수 요청
+@router.post("/super/student_score")
+async def create_room(request: GetStudentScoreRequest):
+    room = rooms.get(request.room_id)
+
+    return room.participants
+
 
 # 게임에서 학생이 정답을 제출할 때
 @router.post("/student_solve")
@@ -109,6 +122,7 @@ async def participant_action(room_id: str = Form(...),
         # 여기서는 받은 이미지를 OCR 돌리고, 돌린 결과 값 가지고 단순 비교를 통해 정오답 판단.
         # 새로운 딕셔너리를 만들고, 키-값으로 저장하는데, 키는 room_id, 값은 그 방에서 선택한 커스텀 문제 "리스트"를 저장.
         # 몇번 문제인지는 어떻게 암? 프론트에서 전달할 수 있나
+
         # # 학생이 제출한 답변을 OCR을 돌리고 있는 GPU 환경으로 전송 및 단어를 순서대로 배열로 받음.
         
         # GPU_SERVER_URL = "http://146.148.75.252:8000/ocr/" 
@@ -124,10 +138,16 @@ async def participant_action(room_id: str = Form(...),
         user_string = "I am pretty."
 
         if user_string == answer: # 정답이면 
-            await manager.send_personal_message("True", room.host_websocket) # True 라는 메세지를 보냄.
+            # await manager.send_personal_message(f"Participant {participant_id} is right", room.host_websocket) # True 라는 메세지를 보냄.
+            room.participants[participant_id] += 100 # 정답이니까 +100 점
+            # 연속 정답일 때 보너스 점수는 어떻게 구현할까.. -> 보류
+
+            await manager.send_personal_message({"participant_id": participant_id, "isAnswer": "correct", "score": room.participants[participant_id]}, room.host_websocket) # True 라는 메세지를 보냄.
+            return {"detail": "정답입니다."}
         else: # 오답이면
-            await manager.send_personal_message("False", room.host_websocket) # True 라는 메세지를 보냄.
-        return {"detail": "Action sent to host"}
+            # await manager.send_personal_message(f"Participant {participant_id} is wrong", room.host_websocket) # True 라는 메세지를 보냄.
+            await manager.send_personal_message({"participant_id": participant_id, "isAnswer": "incorrect", "score": room.participants[participant_id]}, room.host_websocket) # True 라는 메세지를 보냄.
+            return {"detail": "오답입니다."}
     else:
         raise HTTPException(status_code=400, detail="Host is not connected")
 
@@ -139,7 +159,8 @@ async def create_room(request: CreateRoomRequest):
         if room.host_id == request.host_id:
             raise HTTPException(status_code=400, detail="호스트는 하나의 방만 가질 수 있습니다.")
     pin_number = create_pin_number()
-    rooms[pin_number] = Room(pin_number, request.host_id) # 방 생성
+    #방 생성하기 전에 핀번호가 중복되는지 검사해야할듯.
+    rooms[pin_number] = Room(pin_number, request.host_id, request.room_max) # 방 생성
 
     # custom_problem_set = db.query(CustomProblemSet)\
     # .filter(CustomProblemSet.name == set_name)\
@@ -154,6 +175,7 @@ async def create_room(request: CreateRoomRequest):
     return {"detail": "Room created successfully","pin_number": pin_number} # 메세지
 
 # 게임방 참가 api
+# 방에 참가한 상태로 다른 방으로 참가하는 것도 막아야됨
 @router.post("/join_room")
 async def join_room(request: JoinRoomRequest):
     room = rooms.get(request.room_id)
@@ -162,11 +184,17 @@ async def join_room(request: JoinRoomRequest):
     if request.participant_id in room.participants: # 이미 중복된 참여이면
         raise HTTPException(status_code=400, detail="Participant already in the room")
     
-    room.participants.append(request.participant_id) # 방 리스트에 추가. 
+    if len(room.participants) < room.room_max:
+    # room.participants.append(request.participant_id) # 방 리스트에 추가. 
+        room.participants[request.participant_id] = 0 # 방 딕셔너리에 참여자 추가 및 초기값 0 설정 
+    else:
+        return {"message": "Room is full!"}    
+    
     # Notify the host if the host is connected
     if room.host_websocket: # 호스트 웹소켓이 존재하고 (= 호스트가 방을 만들고, 웹소켓으로 연결된 상태이면)
         # await ? 뒤의 작업이 완료될 때 까지 대기함. 
-        await manager.send_personal_message(f"Participant {request.participant_id} has joined the room: {request.room_id}", room.host_websocket)
+        # await manager.send_personal_message(f"Participant {request.participant_id} has joined the room: {request.room_id}", room.host_websocket)
+        await manager.send_personal_message({"participant_id": request.participant_id, "joined_room_id": request.room_id}, room.host_websocket)
     return {"message": "Joined room successfully"}
 
 # 웹소켓 : 이미 생성된 room_id 에 클라이언트를 연결 
@@ -178,6 +206,10 @@ async def websocket_endpoint(websocket: WebSocket, room_id: str, client_id: str)
     if not room: # 만약 없으면
         await websocket.close(code=1000) # code=1000 ? 을 호출하고 닫음 ?
         return
+
+    if len(room.participants) > room.room_max: # 호스트는 인원수에서 제외 ex. > 2 이면 호스트 포함 3명.
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="최대 참가자 수 초과")
+    
 
     if client_id == room.host_id: # 만약 클라이언트가 호스트면, 호스트 웹소켓 ? 에 추가.
         room.host_websocket = websocket
@@ -199,8 +231,12 @@ async def websocket_endpoint(websocket: WebSocket, room_id: str, client_id: str)
 
             for participant_id, participant_ws in manager.active_connections[room_id].items(): # 연결된 게임방의 아이템 ? 만큼 반복 ?
                 if participant_id != client_id: # 참여자 id 와 클라이언트 id가 같지 않으면 (= 자기 자신한테는 메세지 안 보내게 하려고 ? )
-                    await manager.send_personal_message(f"Message from {client_id}: {message_text}", participant_ws) #
+                    # await manager.send_personal_message(f"Message from {client_id}: {message_text}", participant_ws) #
+                    await manager.send_personal_message({"client_id": client_id, "message": message_text}, participant_ws)
     except WebSocketDisconnect:
         manager.disconnect(room_id, client_id)
+        if client_id in room.participants: # 리스트에 참여자가 있다면
+            room.participants.pop(client_id) # 참여자 딕셔너리에서 삭제
+
         if client_id == room.host_id:
             room.host_websocket = None
