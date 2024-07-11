bc89d0d74a656168699e81ef775d2844365324b1
martin <martinjgk@gmail.com>
2024-07-11 15:19:31 +0900

    merge

---
+++ app/src/student/router.py
@@ -0,0 +1,172 @@
+from sqlalchemy.orm import Session, joinedload
+from fastapi import APIRouter, Depends, HTTPException
+from starlette import status
+from database import engine
+from sqlalchemy.orm import Session
+from fastapi.templating import Jinja2Templates
+
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from auth.router import get_current_user, get_user_exception
+from src.models import Users, StudyInfo
+import src.models as models
+from student.dependencies import user_dependency, db_dependency, get_db
+
+router = APIRouter( 
+    prefix="/student",
+    tags=["student"],
+    responses={404: {"description": "Not found"}}
+)
+
+
+models.Base.metadata.create_all(bind=engine)
+templates = Jinja2Templates(directory="templates")
+
+
+# 학생과 선생님 연결 요청, 학생 -> 선생님(student_teachers) / 선생님 -> 학생(teachers_students) ?
+@router.get("/connecting", status_code = status.HTTP_200_OK)
+async def connect_teacher(teacher_id: int,
+            user: dict = Depends(get_current_user),
+            db: Session = Depends(get_db)):
+
+    if user is None:
+        raise get_user_exception()
+    
+    if user.get('user_role') != 'student': # student 인 경우만 
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+
+    # 학생 정보 가져오기
+    student = db.query(Users).filter(Users.id == user.get("id")).first()
+    
+    if not student:
+        raise get_user_exception()
+
+    if teacher_id == user.get("id"):
+        raise HTTPException(status_code=404, detail='자기 자신은 지정할 수 없습니다.')
+    
+    # 쿼리 파라미터로 받은 teacher_id 또는 teacher_username을 사용해 선생님 검색
+    teacher = db.query(Users).filter(Users.id == teacher_id).first()
+
+    if not teacher:
+        raise HTTPException(status_code=404, detail='선생님을 찾을 수 없습니다.')
+
+    # 학생과 선생님이 이미 연결되어 있는지 확인
+    if teacher in student.student_teachers:
+        return {"message": "Already connected"}
+
+    # 새로운 연결 생성
+    student.student_teachers.append(teacher)
+    # db.add(student) # ORM 방식은 append 할 때 자동으로 해줌
+    db.commit()
+    return {"message": "Connected successfully", "teacher_id": teacher.id, "teacher_username": teacher.username}
+
+
+# 학생(self)과 연결된 선생님 아이디 반환
+@router.get("/connect_teacher", status_code = status.HTTP_200_OK)
+async def read_connect_teacher(user: user_dependency, db: db_dependency):
+
+    if user is None:
+        raise get_user_exception()
+    
+    if user.get('user_role') != 'student': # student 인 경우만 
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    
+    # 쿼리 검색
+    teacher = db.query(Users).options( 
+        joinedload(Users.student_teachers)
+    ).filter(Users.id == user.get("id")).first()
+
+    # if not teacher.student_teachers: # 없으면 not found.
+    #     raise http_exception()
+    
+    # {"teachers": [{"id": teacher.id} for teacher in teacher.student_teachers]}
+
+    return {"teachers": [{"id": teacher.id} for teacher in teacher.student_teachers]}
+
+
+# 학생 정보 반환
+@router.get("/info", status_code = status.HTTP_200_OK)
+async def read_user_info(user: user_dependency, db: db_dependency):
+
+    if user is None:
+        raise get_user_exception()
+    
+    if user.get('user_role') != 'student': # student 인 경우만 
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    
+    user_model = db.query(Users).filter(Users.id == user.get('id')).first()
+    return {'name': user_model.name, 'age': user_model.age, 'team_id': user_model.team_id}
+    # 필터 사용. 학습 정보의 owner_id 와 '유저'의 id가 같으면,
+    # 사용자의 모든 정보 반환.
+
+    # Join 해서 학습 정보를 반환해야됨. + 노출되는 정보 필터링
+
+
+# 사용자의 id 반환, self
+@router.get("/id", status_code = status.HTTP_200_OK)
+async def read_user_id(user: user_dependency, db: db_dependency):
+
+    if user is None:
+        raise get_user_exception()
+    
+    if user.get('user_role') != 'student': # student 인 경우만 
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    
+    return  {"id": user.get('id')}
+    # 사용자의 id 반환.
+
+
+# 학생의 self 학습 정보 반환.
+@router.get("/studyinfo", status_code = status.HTTP_200_OK)
+async def read_user_studyinfo(user: user_dependency, db: db_dependency):
+
+    if user is None:
+        raise get_user_exception()
+    
+    if user.get('user_role') != 'student': # student 인 경우만 
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+
+    user_model = db.query(Users.id, Users.username, Users.age, Users.group).filter(Users.id == user.get('id')).first()
+
+    study_info = db.query(StudyInfo).options(
+        joinedload(StudyInfo.correct_problems),
+        joinedload(StudyInfo.incorrect_problems)
+    ).filter(StudyInfo.id == user.get("id")).first()
+
+    # 초기화
+    correct_problems_type1_count = 0
+    correct_problems_type2_count = 0
+    correct_problems_type3_count = 0
+    incorrect_problems_type1_count = 0
+    incorrect_problems_type2_count = 0
+    incorrect_problems_type3_count = 0
+    
+    # 조금 수정을 원해, 매번 확인한다? 조금 그렇긴 해
+    for problem in study_info.correct_problems:
+        if problem.type == '부정문':
+            correct_problems_type1_count += 1
+        elif problem.type == '의문문':
+            correct_problems_type2_count += 1
+        elif problem.type == '단어와품사':
+            correct_problems_type3_count += 1
+
+    for problem in study_info.incorrect_problems:
+        if problem.type == '부정문':
+            incorrect_problems_type1_count += 1
+        elif problem.type == '의문문':
+            incorrect_problems_type2_count += 1
+        elif problem.type == '단어와품사':
+            incorrect_problems_type3_count += 1
+
+    return {
+        'user_id': user_model[0],
+        'name': user_model[1],
+        'age': user_model[2],
+        'class': user_model[3],
+        'type1_True_cnt' : correct_problems_type1_count,
+        'type2_True_cnt' : correct_problems_type2_count,
+        'type3_True_cnt' : correct_problems_type3_count,
+        'type1_False_cnt' : incorrect_problems_type1_count,
+        'type2_False_cnt' : incorrect_problems_type2_count,
+        'type3_False_cnt' : incorrect_problems_type3_count }
---
+++ app/src/user/router.py
@@ -0,0 +1,81 @@
+from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, HTTPException, Path
+from starlette import status
+
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from auth.router import get_current_user
+from src.models import Users
+from user.dependencies import user_dependency, db_dependency, get_db
+from user.schemas import UserQuitVerification, UserVerification, User_info
+from user.utils import bcrypt_context, successful_response
+from user.exceptions import http_exception
+
+router = APIRouter(
+    prefix='/users', 
+    tags=['users']
+)
+
+@router.put("/password", status_code=status.HTTP_200_OK)
+async def change_password(user: user_dependency, db: db_dependency,
+                          user_verification: UserVerification):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    
+    user_model = db.query(Users).filter(Users.id == user.get('id')).first()
+    
+    if not bcrypt_context.verify(user_verification.password, user_model.hashed_password):
+    # 비밀번호 인증.
+        raise HTTPException(status_code=401, detail='기존 비밀번호가 틀렸습니다.')
+    
+    user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
+    # 해시값을 새로운 해시값으로 교체.
+    db.add(user_model)
+    db.commit()
+
+    return {'detail': '비밀번호가 변경되었습니다.'}
+
+@router.put("/update", status_code=status.HTTP_200_OK)
+async def update_user_info(user_info: User_info,
+                      user: dict = Depends(get_current_user),
+                      db: Session = Depends(get_db)):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+
+    user_model = db.query(Users).filter(Users.id == user.get('id')).first()
+
+    if user_model is None:
+        raise http_exception()
+
+    user_model.name = user_info.name
+    user_model.username = user_info.username
+    user_model.phone_number = user_info.phone_number
+    user_model.email = user_info.email
+
+    db.add(user_model)
+    db.commit()
+
+    return successful_response(200)
+
+
+@router.delete("/quit/", status_code=status.HTTP_200_OK)
+async def delete_user(user: user_dependency, db: db_dependency, user_verification: UserQuitVerification):
+
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    
+    user_model = db.query(Users).filter(Users.id == user.get('id')).first()
+    
+    # 비밀번호 인증.
+    if not bcrypt_context.verify(user_verification.password, user_model.hashed_password):
+        raise HTTPException(status_code=401, detail='비밀번호가 틀렸습니다.')
+    
+    user_model = db.query(Users).filter(Users.id == user.get('id')).first()
+    if user_model is None:
+        raise HTTPException(status_code=404, detail='Not found.')
+    
+    db.query(Users).filter(Users.id == user.get('id')).delete()
+    db.commit()
+
+    return {'detail': '성공적으로 탈퇴되었습니다.'}
