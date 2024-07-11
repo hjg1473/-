24dadd117f309b4fdebb2e851bd41343f4532c9b
aiden890 <khmin101@gmail.com>
2024-07-11 17:10:36 +0900

    refactor: add super repository

---
+++ app/src/super/dependencies.py
@@ -0,0 +1,19 @@
+from fastapi import Depends
+from typing import Annotated
+from sqlalchemy.orm import Session
+from database import SessionLocal
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from auth.router import get_current_user
+
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
