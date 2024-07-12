efd4a2332867556c3fc9716cd6daaa0c65dea5d9
martin <martinjgk@gmail.com>
2024-07-12 11:17:45 +0900

    extendedproblems

--- extended_problems.py
+++ extended_problems.py
@@ -11,14 +11,40 @@ genai.configure(api_key=GOOGLE_API_KEY)
 
 model = genai.GenerativeModel('gemini-1.5-flash')
 words = pd.read_csv('data/Vicdream_words_season1.csv')
-words = words['words'].to_numpy().tolist()
+exist_problems = pd.read_csv('data/Vicdream_problems_season1.csv')
+exist_problems = str(exist_problems['Eng'].to_numpy().tolist)
+words = str(words['words'].to_numpy().tolist())
 print(words)
 
 message={
     "role":"user",
-    "parts":{"texts":["Let's think step by step. You are a english teacher, and I am five-year-old student. Generate 20 short sentences only using words in this list.", words, "Put a space in front of every punctuation in generated sentences. Response should be only generated senteces."]}
+    "parts":["Let's think step by step. You are a english teacher, and I am five-year-old student. Generate 20 short sentences only using words in this list.", words, "Check again if there are words not in this list, and if there are, regenarte the detected sentence.", "Put a space in front of every punctuation in generated sentences. Response should be only generated senteces."]
 }
 
 response = model.generate_content(message)
+raw_problems = response.text.split('\n')
+if '' in raw_problems:
+    raw_problems.remove('')
 
-print(response.text)
+print(raw_problems)
+
+problems = []
+
+for i in raw_problems:
+    single_sentence = i.split(' ')
+    while '' in single_sentence:
+        single_sentence.remove('')
+    isWordsOK = True
+    for word in single_sentence:
+        if word not in words:
+            isWordsOK = False
+            break
+    if isWordsOK:
+        sentence = ''
+        for word in single_sentence:
+            sentence += word + ' '
+        
+        problems.append(sentence)
+
+print(len(problems))
+print(problems)
