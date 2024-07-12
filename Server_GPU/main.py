6067d186d4017198a8cb6eda41a4c98560ba5874
hjg1473 <hjg1473@naver.com>
2024-07-12 11:16:20 +0900

    feature/design_pattern/to_aws

--- Server_GPU/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from OCR import ocr
-
-
-app=FastAPI()
-
-app.include_router(ocr.router)
--- app/src/main.py
+++ app/src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-import models
-from database import engine
+from src import models
+from src.database import engine
 from auth import router as auth_router
 from problem import router as problem_router
 from game import router as game_router
