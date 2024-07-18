6822c428429bd88712da95784a7218daed79b555
martin <martinjgk@gmail.com>
2024-07-18 15:41:58 +0900

    practice problem code edit

--- app/src/problem/router.py
+++ app/src/problem/router.py
@@ -38,23 +38,15 @@ async def read_problem_all(user: user_dependency, db: db_dependency):
     # 모든 문제 정보 반환 (일단)
 
 
-# 연습 문제 반환 (10개)
-@router.get("/season/{season_name}/type/{type_name}/practice_set", status_code = status.HTTP_200_OK)
-async def read_problem_all(season_name:str, type_name:str, user: user_dependency, db: db_dependency):
+# 연습 문제 반환
+@router.get("/practice_set/level={level}/step={step}", status_code = status.HTTP_200_OK)
+async def read_problem_all(level:int, step:int, user: user_dependency, db: db_dependency):
     get_user_exception(user)
     
-    if season_name != ('시즌1' or '시즌2'):
-        raise HTTPException(status_code=404, detail='일치하는 시즌이 없습니다. (시즌명 : 시즌1, 시즌2)')
+    # level, step에 해당하는 문제 다 보내기
+    selected_problems = db.query(Problems).filter(Problems.season == level, Problems.problemLevel == step).all()
     
-    if type_name != ('부정문' or '의문문' or '단어와품사'):
-        raise HTTPException(status_code=404, detail='일치하는 유형이 없습니다. (유형 : 부정문, 의문문, 단어와품사)')
-
-    # 시즌과 타입이 같은 모든 문제 반환. 
-    season_type_problem = db.query(Problems).filter(Problems.season == season_name)\
-    .filter(Problems.type == type_name)\
-    .all()
-
-    if season_type_problem is None:
+    if selected_problems is None:
         raise HTTPException(status_code=404, detail='문제 데이터가 존재하지 않습니다.')
 
 
@@ -71,17 +63,6 @@ async def read_problem_all(season_name:str, type_name:str, user: user_dependency
             'id': problem.id
         })
 
-    send_data_to_gpu = {
-        'owner_id': study_info.id,
-        'type1Level': study_info.type1Level,
-        'type2Level': study_info.type2Level,
-        'type3Level': study_info.type3Level,
-        'season': season_name,
-        'type': type_name,
-        'coorect_problems': correct_problems }
-    
-    # requests.post("http://URL/server/calculate_student_level", json=send_data_to_gpu)
-    
     if study_info is None:
         raise http_exception()
     # 받을 때, 업데이트된 유형별 학생 수준만. 
@@ -91,16 +72,16 @@ async def read_problem_all(season_name:str, type_name:str, user: user_dependency
 
 
     # 임의로 앞에서부터 10개 사용
-    select_problem = []
-    cnt = 1
-    for problem in season_type_problem:
-        select_problem.append({'id': problem.id, 'englishProblem': problem.englishProblem, 'koreanProblem': problem.koreaProblem})
-        cnt += 1
-        if cnt == 10:
-            return select_problem
+    # select_problem = []
+    # cnt = 1
+    # for problem in season_type_problem:
+    #     select_problem.append({'id': problem.id, 'englishProblem': problem.englishProblem, 'koreanProblem': problem.koreaProblem})
+    #     cnt += 1
+    #     if cnt == 10:
+    #         return select_problem
 
     # 10개가 안되도 출력 (임시)
-    return select_problem
+    # return select_problem
 
 # # 학생이 문제를 풀었을 때, 일단 임시로 맞았다고 처리 
 # @router.post("/solve", status_code = status.HTTP_200_OK)
