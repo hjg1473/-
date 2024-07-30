96a31b1fa2daf1de19acfc30b1da1d31a8c3df7f
parkjin21004 <parkjin21004@gmail.com>
2024-07-30 16:32:48 +0900

    Feat: add throttle to prevent multiple API request
    - add throttle in student_group_setting_screen in onPinGeneratePressed

--- block_english/lib/screens/SuperScreens/super_group_setting_screen.dart
+++ block_english/lib/screens/SuperScreens/super_group_setting_screen.dart
@@ -1,5 +1,6 @@
 import 'package:block_english/services/super_service.dart';
 import 'package:block_english/widgets/GroupWidget/pin_code_widget.dart';
+import 'package:debounce_throttle/debounce_throttle.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
@@ -25,6 +26,8 @@ class _GroupSettingScreenState extends ConsumerState<GroupSettingScreen>
   String _pinCode = '';
   Ticker? _ticker;
   Duration _elapsed = Duration.zero;
+
+  late Throttle _throttle;
 
   onPinGeneratePressed() async {
     final result =
@@ -63,6 +66,16 @@ class _GroupSettingScreenState extends ConsumerState<GroupSettingScreen>
   }
 
   @override
+  void initState() {
+    super.initState();
+    _throttle = Throttle<String>(
+      const Duration(milliseconds: 5000),
+      initialValue: '',
+      checkEquality: false,
+    )..values.listen((_) => onPinGeneratePressed());
+  }
+
+  @override
   void dispose() {
     _groupNameEditController.dispose();
     super.dispose();
@@ -74,102 +87,106 @@ class _GroupSettingScreenState extends ConsumerState<GroupSettingScreen>
       appBar: AppBar(
         title: const Text('그룹 설정'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              '그룹명 변경',
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            TextField(
-              controller: _groupNameEditController..text = widget.groupName,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                suffixIcon: Icon(
-                  Icons.edit,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                '그룹명 변경',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20),
-              child: SizedBox(
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: _groupNameEditController..text = widget.groupName,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  suffixIcon: Icon(
+                    Icons.edit,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed:
+                        // TODO: add onPressed
+                        () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 65, 65, 65),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                    ),
+                    child: const Text(
+                      '저장하기',
+                      style: TextStyle(color: Colors.white, fontSize: 20.0),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              const Text(
+                '그룹 입장 핀코드 생성',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              SizedBox(
                 width: double.infinity,
-                height: 50,
+                height: 50.0,
                 child: ElevatedButton(
-                  onPressed:
-                      // TODO: add onPressed
-                      () {},
+                  onPressed: (pinCodeExpired || !pinCodeExist)
+                      ? () {
+                          _throttle.setValue(_pinCode);
+                        }
+                      : null,
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 65, 65, 65),
                     shape: RoundedRectangleBorder(
+                      side: const BorderSide(
+                        color: Colors.black,
+                      ),
                       borderRadius: BorderRadius.circular(12.0),
                     ),
+                    backgroundColor: const Color.fromARGB(255, 255, 255, 255),
                   ),
                   child: const Text(
-                    '저장하기',
-                    style: TextStyle(color: Colors.white, fontSize: 20.0),
+                    'PIN 코드 생성하기',
+                    style: TextStyle(fontSize: 20.0),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            const Text(
-              '그룹 입장 핀코드 생성',
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              width: double.infinity,
-              height: 50.0,
-              child: ElevatedButton(
-                onPressed: (pinCodeExpired || !pinCodeExist)
-                    ? onPinGeneratePressed
-                    : null,
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    side: const BorderSide(
-                      color: Colors.black,
-                    ),
-                    borderRadius: BorderRadius.circular(12.0),
+              if (pinCodeExist)
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20.0),
+                  child: PinCodeWidget(
+                    pinCode: _pinCode,
+                    onButtonClicked: () {
+                      setState(() {
+                        _ticker!.stop();
+                        pinCodeExist = false;
+                      });
+                    },
+                    elapsed: _elapsed,
                   ),
-                  backgroundColor: const Color.fromARGB(255, 255, 255, 255),
                 ),
-                child: const Text(
-                  'PIN 코드 생성하기',
-                  style: TextStyle(fontSize: 20.0),
-                ),
-              ),
-            ),
-            if (pinCodeExist)
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20.0),
-                child: PinCodeWidget(
-                  pinCode: _pinCode,
-                  onButtonClicked: () {
-                    setState(() {
-                      _ticker!.stop();
-                      pinCodeExist = false;
-                    });
-                  },
-                  elapsed: _elapsed,
-                ),
-              ),
-          ],
+            ],
+          ),
         ),
       ),
     );
