753711a261084ba663d2bb6cdc2af9e7e1a13b04
martin <martinjgk@gmail.com>
2024-07-30 14:17:24 +0900

    Update router.py

--- app/src/problem/router.py
+++ app/src/problem/router.py
@@ -140,42 +140,61 @@ async def read_problem_all(level:int, step:int, user: user_dependency, db: db_de
 @router.post("/solve", status_code = status.HTTP_200_OK)
 async def user_solve_problem(user: user_dependency, db: db_dependency, problem_id: int = Form(...), file: UploadFile = File(...)):
     get_user_exception(user)
-    user_instance = db.query(Users).filter(Users.id == user.get("id")).first()
 
-    study_info = db.query(StudyInfo).filter(StudyInfo.owner_id == user.get("id")).first()
-    if study_info is None:
-        raise http_exception()
+    # study info 가져오기
+    temp_result = await db.execute(select(StudyInfo).options(joinedload(StudyInfo.correct_problems)).options(joinedload(StudyInfo.incorrect_problems)).filter(StudyInfo.owner_id == user.get("id")))
+    study_info = temp_result.scalars().first()
 
-    # 학생이 제시받은 문제 id와 문제 id 비교해서 문제 찾아냄.
-    problem = db.query(Problems)\
-        .filter(Problems.id == problem_id)\
-        .first()
+    # 문제 id로 문제 가져오기
+    temp_result = await db.execute(select(Problems).filter(Problems.id == problem_id))
+    problem_model = temp_result.scalars().first()
+    problem = problem_model.englishProblem
+    problem_parse = parse_sentence(problem)
 
-    # 학생이 제출한 답변을 OCR을 돌리고 있는 GPU 환경으로 전송 및 단어를 순서대로 배열로 받음.
-    GPU_SERVER_URL = "http://146.148.75.252:8000/ocr/" 
-    
+    # OCR로 response 읽어오기
     img_binary = await file.read()
-    file.filename = "img.png"
-    files = {"file": (file.filename, img_binary)}
-    user_word_list = requests.post(GPU_SERVER_URL, files=files)
-    
-    user_string = " ".join(user_word_list.json())
+    image = await asyncio.to_thread(Image.open,io.BytesIO(img_binary))
+    # image = Image.open(io.BytesIO(img_binary))
+    img_array = np.array(image)
+    from app.src.main import reader
+    result = await asyncio.to_thread(reader.readtext, img_array, allowlist='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!,?.', text_threshold=0.4,low_text=0.3)
 
-    #answer = problem.englishProblem
-    answer = "I am pretty"
-    
-    # 문제를 맞춘 경우, correct_problems에 추가. id 만 추가. > 하고 싶은데 안되서 일단 problem 전체 저장함.
-    # 일단 정답인 경우만 구현, 문장이 다르면 오답처리
-    isAnswer, false_location, score = check_answer(answer, user_string)
+    # 1. 각 사각형의 높이 구하기
+    heights = []
+    for item in result:
+        coords = item[0]
+        y_values = [point[1] for point in coords]
+        height = max(y_values) - min(y_values)
+        heights.append(height)
+
+    # 높이의 최댓값 구하기
+    max_height = max(heights)
+
+    # 2. 높이의 최댓값의 0.7배 이하 무시하기
+    threshold = max_height * 0.7
+    filtered_data = [item for item, height in zip(result, heights) if height > threshold]
+
+    # 3. 남은 단어들을 x축 오름차순으로 정렬해서 단어 리스트 만들기
+    sorted_data = sorted(filtered_data, key=lambda item: min(point[0] for point in item[0]))
+    response_parse = [item[1] for item in sorted_data]
+
+    # 채점
+    isAnswer, false_location = check_answer(problem_parse, response_parse)
+
     if isAnswer:
-        study_info.correct_problems.append(problem)
-
+        study_info.correct_problems.append(problem_model)
+        await increment_correct_problem_count(study_info.id, problem_model.id, db)
+        db.add(study_info)
+        await db.commit()
+        result = {"you did good job"}
     else:
-        study_info.incorrect_problems.append(problem)
+        study_info.incorrect_problems.append(problem_model)
+        result = await calculate_wrong_info(problem_parse, response_parse, db)
     db.add(study_info)
-    db.commit()
+    await db.commit()
 
-    return {'isAnswer' : problem.englishProblem, 'user_answer': user_string, 'false_location': false_location}
+    return result    
+
 
 # 학생이 문제를 풀었을 때, 일단 임시로 맞았다고 처리 
 @router.post("/solve_test_problem_count", status_code = status.HTTP_200_OK)
