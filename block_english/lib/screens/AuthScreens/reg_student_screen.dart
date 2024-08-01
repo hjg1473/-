acc1f59ca11c53ae3d0089d8a1d844c8f0c9773f
Woo Yeonseo <ysyskr@g.skku.edu>
2024-08-01 09:42:31 +0900

    Rename: Move square button to file and reg super screen

--- block_english/lib/screens/AuthScreens/reg_student_screen.dart
+++ block_english/lib/screens/AuthScreens/reg_student_screen.dart
@@ -1,6 +1,7 @@
 import 'package:block_english/services/auth_service.dart';
 import 'package:block_english/utils/device_scale.dart';
 import 'package:block_english/widgets/reg_input_box.dart';
+import 'package:block_english/widgets/square_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -284,36 +285,6 @@ class _StudState extends ConsumerState<RegStudentScreen> {
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class SquareButton extends StatelessWidget {
-  final String text;
-  final Color? backgroundColor;
-  final VoidCallback? onPressed;
-
-  const SquareButton({
-    super.key,
-    required this.text,
-    this.backgroundColor = Colors.black,
-    this.onPressed,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return FilledButton(
-      onPressed: onPressed,
-      style: FilledButton.styleFrom(
-        minimumSize:
-            Size(double.infinity, DeviceScale.squareButtonHeight(context)),
-        backgroundColor: backgroundColor,
-        shape: const BeveledRectangleBorder(),
-      ),
-      child: Text(
-        text,
-        style: const TextStyle(fontSize: 18),
       ),
     );
   }
