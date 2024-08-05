ed57d17383003222a58cabbde2bdccb28238dd37
parkjin21004 <parkjin21004@gmail.com>
2024-08-05 14:42:45 +0900

    Remove: remove screenutil from whole project

--- block_english/lib/main.dart
+++ block_english/lib/main.dart
@@ -15,7 +15,6 @@ import 'package:block_english/utils/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -33,37 +32,34 @@ class MyApp extends StatelessWidget {
 
     SizeConfig().init(context);
 
-    return ScreenUtilInit(
-      designSize: const Size(812, 375),
-      builder: (context, child) => MaterialApp(
-        theme: ThemeData(
-          scaffoldBackgroundColor: Colors.white,
-          appBarTheme: const AppBarTheme(
-            backgroundColor: Colors.white,
-            scrolledUnderElevation: 0,
-          ),
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          scrolledUnderElevation: 0,
         ),
-        title: "Block English",
-        initialRoute: '/loading_screen',
-        routes: {
-          '/loading_screen': (context) => const LoadingScreen(),
-          '/login_screen': (context) => const LoginScreen(),
-          '/reg_select_role_screen': (context) => const RegSelectRoleScreen(),
-          '/reg_student_screen': (context) => const RegStudentScreen(),
-          '/reg_super_screen': (context) => const RegSuperScreen(),
-          '/stud_mode_select_screen': (context) =>
-              const StudentModeSelectScreen(),
-          '/stud_season_select_screen': (context) =>
-              const StudentSeasonSelectScreen(),
-          '/stud_main_screen': (context) => const StudentMainScreen(),
-          '/super_main_screen': (context) => const SuperMainScreen(),
-          '/super_group_create_screen': (context) =>
-              const SuperGroupCreateScreen(),
-          '/super_game_code_screen': (context) => const SuperGameCodeScreen(),
-          '/super_game_screen': (context) => const SuperGameScreen(),
-          '/setting_screen': (context) => const SettingScreen(),
-        },
       ),
+      title: "Block English",
+      initialRoute: '/loading_screen',
+      routes: {
+        '/loading_screen': (context) => const LoadingScreen(),
+        '/login_screen': (context) => const LoginScreen(),
+        '/reg_select_role_screen': (context) => const RegSelectRoleScreen(),
+        '/reg_student_screen': (context) => const RegStudentScreen(),
+        '/reg_super_screen': (context) => const RegSuperScreen(),
+        '/stud_mode_select_screen': (context) =>
+            const StudentModeSelectScreen(),
+        '/stud_season_select_screen': (context) =>
+            const StudentSeasonSelectScreen(),
+        '/stud_main_screen': (context) => const StudentMainScreen(),
+        '/super_main_screen': (context) => const SuperMainScreen(),
+        '/super_group_create_screen': (context) =>
+            const SuperGroupCreateScreen(),
+        '/super_game_code_screen': (context) => const SuperGameCodeScreen(),
+        '/super_game_screen': (context) => const SuperGameScreen(),
+        '/setting_screen': (context) => const SettingScreen(),
+      },
     );
   }
 }
